a1337ad982f9698b21d06a5a7ccbb25698683b47
Ona Kotryna <ok.kuncyte@gmail.com>
2023-09-10 13:27:46 +0300

    Add card canvas

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from tkinter import *
 
-
+BG_COLOR="#9bdeac"
+IMG_WIDTH=800
+IMG_HEIGHT=526
 window = Tk()
 window.title("Flash Cards")
-window.config(padx=50, pady=50)
+window.config(padx=50, pady=50, bg=BG_COLOR)
 
+canvas = Canvas(width=IMG_WIDTH, height=IMG_HEIGHT, highlightthickness=0, bg=BG_COLOR)
+card_front = PhotoImage(file="./images/card_front.png")
+canvas.create_image(IMG_WIDTH/2, IMG_HEIGHT/2, image=card_front)
+canvas.grid(row=0, column=0, columnspan=2)
 
 window.mainloop()
