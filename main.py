a971edc4eab8e077cf8b1a7c82e9faada179e505
Ona Kotryna <ok.kuncyte@gmail.com>
2023-09-10 13:44:52 +0300

    Add action buttons

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from tkinter import *
 
-BG_COLOR="#9bdeac"
+BG_COLOR="#B1DDC6"
 IMG_WIDTH=800
 IMG_HEIGHT=526
 
 ORIGIN_LANGUAGE="German"
 TRANSLATION_LANGUAGE="English"
 
+
+# ---------------------- BUTTON ACTIONS ----------------------
+
+def set_right():
+    print("Answer was right")
+
+def set_wrong():
+    print("Answer was wrong")
+
+# ---------------------------- UI ----------------------------
 window = Tk()
 window.title("Flash Cards")
 window.config(padx=50, pady=50, bg=BG_COLOR)
@@ -18,5 +28,13 @@ canvas.grid(row=0, column=0, columnspan=2)
 
 text_language = canvas.create_text(400, 150, text=ORIGIN_LANGUAGE, font=("Arial", 40, "italic"))
 text_word = canvas.create_text(400, 263, text="word", font=("Arial", 60, "bold"))
+
+img_wrong = PhotoImage(file="./images/wrong.png")
+btn_wrong = Button(image=img_wrong, highlightthickness=0, bd=0, command=set_wrong)
+btn_wrong.grid(row=1, column=0)
+
+img_right = PhotoImage(file="./images/right.png")
+btn_rigth = Button(image=img_right, highlightthickness=0, bd=0, command=set_right)
+btn_rigth.grid(row=1, column=1)
 
 window.mainloop()
