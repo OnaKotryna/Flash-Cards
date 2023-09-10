29d340183fb751b5e495f625519744d3a84a66f9
Ona Kotryna <ok.kuncyte@gmail.com>
2023-09-10 14:38:12 +0300

    Add data logic

---
+++ data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+csv_data = "./data/de_1k.csv"
+df = pd.read_csv(csv_data)
+
+
+def get_languages():
+    return df.columns.tolist()
+
+
+def get_word():
+    words = df.sample(n=1)
+    return words.to_dict(orient="records")
